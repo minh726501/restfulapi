70771fc1422e6996cff60ce0b417e36aedac2092
minh726501 <buiminh272002@gmail.com>
2025-06-09 22:45:33 +0900

    crud company

--- src/main/java/com/restfulapi/controller/CompanyController.java
+++ src/main/java/com/restfulapi/controller/CompanyController.java
@@ -1,13 +1,17 @@
 package com.restfulapi.controller;
 
 import com.restfulapi.entity.Company;
+import com.restfulapi.exception.ResourceNotFoundException;
 import com.restfulapi.service.CompanyService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CompanyController {
@@ -21,5 +25,33 @@ public class CompanyController {
     public ResponseEntity<Company>newCompany(@RequestBody @Valid Company company){
         Company newCompany=companyService.saveCompany(company);
         return ResponseEntity.status(HttpStatus.CREATED).body(newCompany);
+    }
+    @GetMapping("companies")
+    public ResponseEntity<List<Company>>getAllCompany(){
+        return ResponseEntity.ok().body(companyService.getAllCompany());
+    }
+    @GetMapping("companies/{id}")
+    public Company getCompanyById(@PathVariable long id){
+        return companyService.getCompanyById(id).orElseThrow(() -> new ResourceNotFoundException(("Không tìm thấy Company với ID: " + id)));
+    }
+    @PutMapping("companies")
+    public ResponseEntity<?> updateCompany(@RequestBody Company company){
+        Optional<Company> getCompany=companyService.getCompanyById(company.getId());
+        if (getCompany.isPresent()){
+            Company updateCompany=getCompany.get();
+            updateCompany.setName(company.getName());
+            updateCompany.setDescription(company.getDescription());
+            updateCompany.setAddress(company.getAddress());
+            updateCompany.setUpdatedAt(Instant.now());
+            updateCompany.setUpdatedBy(SecurityContextHolder.getContext().getAuthentication().getName());
+            companyService.saveCompany(updateCompany);
+            return ResponseEntity.ok(updateCompany);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Không tìm thấy Company với ID: " + company.getId());
+    }
+    @DeleteMapping("/companies/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable long id) {
+        companyService.deleteCompany(id);
+        return ResponseEntity.ok("delete Company By id" + id);
     }
 }
--- src/main/java/com/restfulapi/service/CompanyService.java
+++ src/main/java/com/restfulapi/service/CompanyService.java
@@ -1,7 +1,12 @@
 package com.restfulapi.service;
 import com.restfulapi.entity.Company;
 import com.restfulapi.repository.CompanyRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CompanyService {
@@ -11,6 +16,17 @@ public class CompanyService {
         this.companyRepository = companyRepository;
     }
     public Company saveCompany(Company company){
+        company.setCreatedBy(SecurityContextHolder.getContext().getAuthentication().getName());
+        company.setCreatedAt(Instant.now());
         return companyRepository.save(company);
+    }
+    public List<Company> getAllCompany(){
+        return companyRepository.findAll();
+    }
+    public Optional<Company> getCompanyById(long id){
+        return companyRepository.findById(id);
+    }
+    public void deleteCompany(long id){
+        companyRepository.deleteById(id);
     }
 }
